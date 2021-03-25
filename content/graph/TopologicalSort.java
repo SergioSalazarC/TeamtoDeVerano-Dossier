6b26cd941105d34011107cff6f4488a906f3610e
serxu <59147261+serxu@users.noreply.github.com>
2021-03-25 14:30:46 +0100

    add tps

---
+++ content/graph/TopologicalSort.java
@@ -0,0 +1,35 @@
+/**
+ * Author: Sergio
+ * Date: 2021
+ * License: CC0
+ * Description: Orden en el que realizar n tareas si 1->2 implica que para hacer 2 hace falta hacer 1
+ * Time: O(E + V)
+ * Status: Tested on:
+ */
+public class TopologicalSort {
+    public static int n; //vertices
+    public static ArrayList<Integer> list;
+    public static boolean visitados[];
+    public static ArrayList<Integer>[] graf;
+
+    public static void dfs_tps(int u){
+        visitados[u]=true;
+        for (Integer k : graf[u]) {
+            if(!visitados[k]){
+                dfs_tps(k);
+            }
+        }
+        list.add(u+1);
+    }
+
+    public static void main(String[] args) {
+        for(int i=0;i<n;i++){
+            if(!visitados[i])
+                dfs_tps(i);
+        }
+        //Recorrido en orden inverso
+        for(int i=list.size()-1;i>=0;i--){
+            System.out.println(list.get(i));
+        }
+    }
+}
