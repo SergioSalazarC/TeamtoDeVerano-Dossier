9661d0f44ca61207a98b8eb4f18e550b756b7771
SergioSalazarC <59147261+SergioSalazarC@users.noreply.github.com>
2022-02-18 23:09:32 +0100

    Update BinomialModPrime.cpp

--- content/math/BinomialModPrime.cpp
+++ content/math/BinomialModPrime.cpp
@@ -1,22 +1,23 @@
 /**
- * Author: Håkan Terelius
- * Date: 2009-09-25
+ * Author: Sergio Salazar
+ * Date: 18/02/2022
  * License: CC0
  * Source: http://en.wikipedia.org/wiki/Lucas'_theorem
- * Description: Lucas' thm: Let $n,m$ be non-negative integers and $p$ a prime.
- * Write $n=n_kp^k+...+n_1p+n_0$ and $m=m_kp^k+...+m_1p+m_0$.
- * Then $\binom{n}{m} \equiv \prod_{i=0}^k\binom{n_i}{m_i} \pmod{p}$.
- * fact and invfact must hold pre-computed factorials / inverse factorials, e.g. from ModInverse.h.
+ * Description: Tª de Lucas: SeaN $n,m$ enteros no negativos y $p$ un primo.
+ * Sea $n=n_kp^k+...+n_1p+n_0$ y $m=m_kp^k+...+m_1p+m_0$.
+ * Entonces $\binom{n}{m} \equiv \prod_{i=0}^k\binom{n_i}{m_i} \pmod{p}$.
+ * fact y invfact deben ser calculados como el factorial y el factorial invertido, por ejemplo desde ModInverse.h.
  * Status: Untested
  * Time: O(\log_p n)
  */
 
-ll chooseModP(ll n, ll m, int p, vi& fact, vi& invfact) {
-	ll c = 1;
+static long chooseModP(long n, long m, int p) {
+	long c = 1;
 	while (n || m) {
-		ll a = n % p, b = m % p;
+		long a = n % p
+		long b = m % p;
 		if (a < b) return 0;
-		c = c * fact[a] % p * invfact[b] % p * invfact[a - b] % p;
+		c = c * fact(a) % p * invfact(b) % p * invfact(a - b) % p;
 		n /= p; m /= p;
 	}
 	return c;
