843803269b7dc5b1e53246be5fc8baa810629cb9
SergioSalazarC <59147261+SergioSalazarC@users.noreply.github.com>
2022-03-03 14:14:52 +0100

    Update ModInverse.java

--- content/number-theory/ModInverse.java
+++ content/number-theory/ModInverse.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /**
  * Author: Francisco Tórtola
  * Date: 2022
@@ -33,39 +33,3 @@ public class ModInverse {
         return x;
     }
 }
-=======
-/**
- * Author: Francisco Tórtola
- * Date: 2022
- * License: CC0
- * Description: Calcula x tal que $a*x = 1 mod m$ a y m son coprimos
- * Time: O(log(m))
- * Status: Tested on:
- */
-public class ModInverse {
-    // Returns modulo inverse of a with respect to m using extended Euclid
-    // Algorithm Assumption: a and m are coprimes, i.e., gcd(a, m) = 1
-    static int modInverse(int a, int m)
-    {
-        int m0 = m;
-        int y = 0, x = 1;
-        if (m == 1) return 0;
-        while (a > 1) {
-            // q is quotient
-            int q = a / m;
-            int t = m;
-            // m is remainder now, process same as Euclid's algo
-            m = a % m;
-            a = t;
-            t = y;
-            // Update x and y
-            y = x - q * y;
-            x = t;
-        }
-        // Make x positive
-        if (x < 0)
-            x += m0;
-        return x;
-    }
-}
->>>>>>> 0ab15d0d33903b8c7e4a1b0fa66eca7e2aea4491
